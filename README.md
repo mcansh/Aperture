97cad780e5e25c95c62b1dcfd442f2540d553eee
Logan McAnsh <l.m@loganmcansh.com>
2016-03-15 21:58:45 -0400

    update readme with image, disable scroll when navigation is open
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Aperture
 
 found a really old [dribbble concept](https://raw.githubusercontent.com/mcansh/aperture/gh-pages/Aperture-Landing-Page.png) on my external hard drive by [@tommysadlr](https://twitter.com/tommysadlr) and decided to make it
+
+![aperture](https://raw.githubusercontent.com/mcansh/aperture/gh-pages/Aperture-Landing-Page.png)
--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,12 @@
 var $hamburger = $(".hamburger");
 var $links = $(".links");
 var $nav = $("nav");
+var $scroll = $("body");
 $hamburger.on("click", function(e) {
   $hamburger.toggleClass("is-active");
   // Do something else, like open/close menu
   $links.toggleClass("is-open");
   $links.toggleClass("blur");
   $nav.toggleClass("blur")
+  $scroll.toggleClass("no-scroll")
 });
